710ceedc892bd5365c62ce6114347c3e77ab4463
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 15:58:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,11 +9,16 @@ class Anagram
   end 
   
   def match(words)
+    ans = []
     wordcomp = @word.sort.split("")
     word_arr = words.split(" ")
     word_arr.detect do |wrd|
       if wordcomp == wrd.sort.split("")
-        
+        wrd << ans
+      end 
+    end
+    ans
+  end 
       
       
     
