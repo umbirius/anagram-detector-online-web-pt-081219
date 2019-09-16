c7bf8ab0ebd28379ceb19a9510eb72d8b31efa9c
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 16:03:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   
   def match(word_array)
     word_array.select do |word|
-      word_array.split("").sort == @word.split("").sort
+      word.split("").sort == @word.split("").sort
     end 
   end 
       
